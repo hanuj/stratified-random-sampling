e6ae9d516ae55c42ea2678e07d7f9f6643d947d1
Harsh Anuj <harsh.anuj@gmail.com>
2018-11-14 23:59:01 -0500

    Update stratRandomSample.R

--- stratRandomSample.R
+++ stratRandomSample.R
@@ -1,6 +1,10 @@
 stratRandomSample<-function(dataframe,columnName,size){
   
-        if(size>1){stop("size has to be less than or equal to 1")}
+  if(size>1){
+    
+    stop("size has to be less than or equal to 1")
+  
+  }
 
   dataframe<-as.data.frame(dataframe)
   
@@ -12,20 +16,32 @@ stratRandomSample<-function(dataframe,columnName,size){
   
   a_size<-length(a)
   
-       if(a_size==1){stop("selected column has only 1 level")}
+  if(a_size==1){
+    
+    stop("selected column has only 1 level")
+  
+  }
   
   factor_sizes<-data.frame()
   
-       for (i in 1:a_size){
-                            factor_sizes[i,"name"]<-a[i]
+  for (i in 1:a_size){
+    
+    factor_sizes[i,"name"]<-a[i]
                               
-                            factor_sizes[i,"num"]<-length(dataframe[which(dataframe[,columnName]==a[i]),columnName])
-                           }
+    factor_sizes[i,"num"]<-length(dataframe[which(dataframe[,columnName]==a[i]),columnName])
+                            
+   }
   
-       for (i in 1:a_size){factor_sizes[i,"relative"]<-round(factor_sizes[i,"num"]/sum(factor_sizes[,"num"]),4)}
+  for (i in 1:a_size){
+    
+    factor_sizes[i,"relative"]<-round(factor_sizes[i,"num"]/sum(factor_sizes[,"num"]),4)
+  
+  }
   
   factor_sizes<-factor_sizes[order(factor_sizes[,"num"]),]
+  
   print("Original data")
+  
   print(factor_sizes)
   
   data_out<-data.frame() 
